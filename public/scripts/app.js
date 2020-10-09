811147e3cabbb2df50b6d5954c314a56f07a7f0a
Gurkirat Khaira <777gurkirat@gmail.com>
2020-10-08 23:27:33 -0400

    updated home, contact and about page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,32 @@
 // IIFE
 (function () {
     function Start() {
+
+        if (document.title === "Contact Me") {
+            let submitButton = document.getElementById("submit");
+            let cancelButton = document.getElementById("reset");
+            let name = document.getElementById("name");
+            let contact = document.getElementById("number");
+            let email = document.getElementById("email");
+            let message = document.getElementById("message");
+
+            submitButton.addEventListener('click', (event) => {
+                event.preventDefault();
+                if (confirm(`The following values will be submitted:
+                Name: ${name.value}
+                Email: ${email.value}
+                Contact: ${contact.value}
+                Message: ${message.value}`)) {
+                    location.href = "/home";
+                }
+            });
+            cancelButton.addEventListener('click', (event) => {
+                event.preventDefault();
+                if (confirm("Are you sure you want to reset the details filled?")) {
+                    location.href = "/home";
+                }
+            });
+        }
         console.log("App started...");
     }
     window.addEventListener("load", Start);
--- routes/index.js
+++ routes/index.js
@@ -4,43 +4,43 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
-    title: 'Home'
+    title: 'Home', file: 'home'
   });
 });
 
 /* GET home page. */
 router.get('/home', function (req, res, next) {
   res.render('index', {
-    title: 'Home'
+    title: 'Home', file: 'home'
   });
 });
 
-/* GET about us page. */
+/* GET about me page. */
 router.get('/about', function (req, res, next) {
   res.render('index', {
-    title: 'About Us'
+    title: 'About Me', file: 'about' 
   });
 });
 
-/* GET product page. */
-router.get('/products', function (req, res, next) {
+/* GET projects page. */
+router.get('/projects', function (req, res, next) {
   res.render('index', {
-    title: 'Products'
+    title: 'Projects', file: 'projects'
   });
 });
 
 /* GET services page. */
 router.get('/services', function (req, res, next) {
   res.render('index', {
-    title: 'Services'
+    title: 'Services', file: 'services'
   });
 });
 
 
-/* GET contact us page. */
+/* GET contact me page. */
 router.get('/contact', function (req, res, next) {
-  res.render('index', {
-    title: 'Contact Us'
+  res.render('contact', {
+    title: 'Contact Me'
   });
 });
 
